fe151e6f72ecdcf621aed186cd6734c5ba85c4ca
luigidamico100 <49816085+luigidamico100@users.noreply.github.com>
2022-07-16 16:31:59 +0200

    annotation file .csv creation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,36 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from src.paths import (
+    path_raw_wav_original,
+    path_raw_images_original,
+    path_annotation_wav_original,
+    path_annotation_images_original,
+)
+import os
+import pandas as pd
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def create_df_annotation(path_orig):
+    list_dict = []
+    sample_dict = {}
+    for genre in os.listdir(path_orig):
+        genre_fullpath = os.path.join(path_orig, genre)
+        for filename in os.listdir(genre_fullpath):
+            fullpath = os.path.join(genre_fullpath, filename)
+            dict_sample = {'genre': genre, 'path': fullpath, 'filename': filename}
+            list_dict.append(dict_sample)
+
+    df_annotation = pd.DataFrame(list_dict)
+    return df_annotation
+
+
+def main():
+    df_annotation_wav = create_df_annotation(path_raw_wav_original)
+    df_annotation_images = create_df_annotation(path_raw_images_original)
+
+    print(f'Writing annotation .wav files to: {path_annotation_wav_original}')
+    df_annotation_wav.to_csv(path_annotation_wav_original)
+    print(f'Writing annotation images files to: {path_annotation_images_original}')
+    df_annotation_images.to_csv(path_annotation_images_original)
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
