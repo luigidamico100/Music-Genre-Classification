ae430618061bfa9b58db373fccdd9fdab775f6ec
luigidamico100 <49816085+luigidamico100@users.noreply.github.com>
2022-07-16 13:53:19 +0200

    cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Music genre classification using CNN',
+    author='Luigi DAmico',
+    license='',
+)
