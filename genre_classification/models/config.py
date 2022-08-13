ce9f611c04205544aeb828cad6070f78e47b402d
luigidamico100 <49816085+luigidamico100@users.noreply.github.com>
2022-08-14 00:38:05 +0200

    Added more argument to parse for training

--- genre_classification/models/config.py
+++ genre_classification/models/config.py
@@ -22,8 +22,8 @@ chunks_len_sec = 14.
 
 # Mel spectrogram params
 melspec_fft = 1024
-melspec_hop_length = 512
-melspec_n_mels = 64
+melspec_hop_length = 256 #512
+melspec_n_mels = 128 #64
 
 # Evaluation params
 set_ = 'val'
@@ -67,23 +67,36 @@ def parse_params(config, reason='training'):
         parser.add_argument('--epochs', type=int, help='epochs number', default=config.epochs)
         # parser.add_argument('--train_debug_mode', type=bool, help='Train debug mode', default=train_debug_mode, action=argparse.BooleanOptionalAction)
         parser.add_argument('--train_debug_mode', type=str, help='Train debug mode', default=config.train_debug_mode)
+        parser.add_argument('--batch_size', type=int, help='batch size', default=config.batch_size)
         parser.add_argument('--learning_rate', type=float, help='training learning rate', default=config.learning_rate)
         parser.add_argument('--experiment_name', type=str, help='experiment name', default=config.experiment_name)
         parser.add_argument('--chunks_len_sec', type=float, help='Chunks length (sec)', default=config.chunks_len_sec)
+        parser.add_argument('--melspec_fft', type=int, help='melspec fft', default=config.melspec_fft)
+        parser.add_argument('--melspec_hop_length', type=float, help='melspec hop length', default=config.melspec_hop_length)
+        parser.add_argument('--melspec_n_mels', type=float, help='melspec n mels', default=config.melspec_n_mels)
+        
         args = parser.parse_args()
         train_debug_mode = args.train_debug_mode == 'True'
         learning_rate = args.learning_rate
+        batch_size = args.batch_size
         experiment_name = args.experiment_name
         chunks_len_sec = args.chunks_len_sec
         n_examples = 'all' if not train_debug_mode else 50
         epochs = args.epochs if not train_debug_mode else 3
+        melspec_fft = args.melspec_fft
+        melspec_hop_length = args.melspec_hop_length
+        melspec_n_mels = args.melspec_n_mels
         
         params['train_debug_mode'] = train_debug_mode
         params['n_examples'] = n_examples
         params['experiment_name'] = experiment_name
         params['epochs'] = epochs
+        params['batch_size'] = batch_size
         params['learning_rate'] = learning_rate
         params['chunks_len_sec'] = chunks_len_sec
+        params['melspec_fft'] = melspec_fft
+        params['melspec_hop_length'] = melspec_hop_length
+        params['melspec_n_mels'] = melspec_n_mels
         
         return params
     
