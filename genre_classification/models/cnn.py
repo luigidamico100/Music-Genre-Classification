b31c7fbabcec8b335863c24c6f36fe676c4559ab
luigidamico100 <49816085+luigidamico100@users.noreply.github.com>
2022-08-15 19:44:30 +0200

    adding dropout

--- genre_classification/models/cnn.py
+++ genre_classification/models/cnn.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 
 class Conv_2d(nn.Module):
-    def __init__(self, input_channels, output_channels, shape=3, pooling=2, dropout=0.):
+    def __init__(self, input_channels, output_channels, shape=3, pooling=2, dropout=0.1):
         super(Conv_2d, self).__init__()
         self.conv = nn.Conv2d(input_channels, output_channels, shape, padding=shape//2)
         self.bn = nn.BatchNorm2d(output_channels)
