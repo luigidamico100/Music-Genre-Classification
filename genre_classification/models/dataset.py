f6a85f6daf45a7c6296e251fa807f62ee8a13e19
luigidamico100 <49816085+luigidamico100@users.noreply.github.com>
2022-08-18 13:15:42 +0200

    getitem refactoring

--- genre_classification/models/dataset.py
+++ genre_classification/models/dataset.py
@@ -19,94 +19,39 @@ from torchaudio_augmentations import (
 )
 
 
-
-class GTZANDataset(Dataset):
-
-    def __init__(self,
-                 path_annotations_file,
-                 path_class_to_genre_map,
-                 path_genre_to_class_map,
-                 n_examples='all',
-                 target_sample_rate=None,
-                 device=None,
-                 folds=[0, 1, 2, 3, 4, 5],
-                 training=True,
-                 chunks_len_sec=7.,
-                 verbose_sample_wasting=False,
-                 return_wav_filename=False,
-                 mel_spectrogram_kwargs=None):
-
-        self.annotations = pd.read_csv(path_annotations_file, index_col=0)
-        with open(path_class_to_genre_map, 'rb') as f:
-            self.class_to_genre_map = pickle.load(f)
-        with open(path_genre_to_class_map, 'rb') as f:
-            self.genre_to_class_map = pickle.load(f)
-        
-        self.genres = list(self.genre_to_class_map.keys())
-        self.annotations = self.annotations[self.annotations['fold'].isin(folds)]
-        if n_examples!='all':
-            self.annotations = self.annotations.sample(n_examples, random_state=42)
-        self.training = training
-        self.device = device
-        self.target_sample_rate = target_sample_rate
-        self.num_samples = int(self.target_sample_rate * chunks_len_sec)
-        #self.transformation = transformation.to(device)
-        self.verbose_sample_wasting = verbose_sample_wasting
-        if self.training:
-            self._get_augmentations()
-        
-        self.mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-            sample_rate=target_sample_rate,
-            normalized=False,
-            **mel_spectrogram_kwargs,).to(device)
-        self.amplitude_to_db = torchaudio.transforms.AmplitudeToDB().to(device)
-        self.return_wav_filename = return_wav_filename
-
-    def __len__(self):
-        return len(self.annotations)
-
-    def __getitem__(self, idx):
-        '''
-        self.training == True
-            return torch.Tensor of shape (f, t).
-                f = n_fft
-                t = (self.num_samples) / hop_length
-        self.training == False
-            return torch.Tensor of shape (c, f, t)
-        
-        c = n chunks
-        f = n frequency bins
-        t = n time bins
-        
-        When used with dataloader, batch dimension b has to be added!
-        '''
-        sample_path = self.annotations.iloc[idx]['wav_path']
-        genre = self.annotations.iloc[idx]['genre']
-        label = self.genre_to_class_map[genre]
-        signal, sr = torchaudio.load(sample_path)
-        signal = signal.to(self.device)
-        signal = self.resample(signal, sr)
-        signal = self.mix_down(signal)
-        #signal = self.adjust_audio_len(signal)
-        # signal = self.cut(signal)
-        # signal = self.right_pad(signal)
-        if self.training:
-            # signal = signal.unsqueeze(0)
-            # #signal = signal.to('cpu')
-            # signal = self.augmentation(signal)
-            # #signal = signal.to(self.device)
-            # signal = signal.squeeze(0)
-            
-            signal = self.adjust_audio_len(signal)
-        else:
-            signal = self.get_signal_chunks(signal)
-        signal = self.mel_spectrogram(signal)
-        signal = self.amplitude_to_db(signal)
-        
-        if self.return_wav_filename:
-            wav_filename = self.annotations.iloc[idx]['wav_filename']
-            return signal, label, wav_filename
-        return signal, label
+def read_wav_and_preprocess(wav_file_path,
+                            wav_to_spec_transformation,
+                            target_sample_rate,
+                            num_samples,
+                            training=False,
+                            verbose_sample_wasting=False,
+                            device='cpu'):
+    
+    
+    def resample(signal, sr, target_sample_rate):
+        if sr != target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, target_sample_rate)
+            signal = resampler(signal)
+        return signal
+    
+    def mix_down(signal):
+        signal = torch.mean(signal, dim=0, keepdim=False)
+        return signal
+    
+    def adjust_audio_len(signal, num_samples):
+        random_index = random.randint(0, len(signal) - num_samples - 1)
+        signal = signal[random_index : random_index + num_samples]
+        return signal
+    
+    def get_signal_chunks(signal, num_samples, target_sample_rate):
+        num_chunks = len(signal) // num_samples
+        len_pre = len(signal)
+        signal = signal[:num_chunks*num_samples]
+        len_post = len(signal)
+        signal = torch.reshape(signal, (num_chunks, num_samples))
+        if verbose_sample_wasting:
+            print(f'Losed {len_pre - len_post} samples ({((len_pre-len_post)/target_sample_rate):.1f}) sec')
+        return signal
     
     def _get_augmentations_pytorch(self):
         from torch import nn
@@ -138,35 +83,117 @@ class GTZANDataset(Dataset):
             #RandomApply([Reverb(sample_rate=22050)], p=0.3).to(self.device),
         ]
         self.augmentation = Compose(transforms=transforms)
-
-
-    def get_signal_chunks(self, signal):
-        num_chunks = len(signal) // self.num_samples
-        len_pre = len(signal)
-        signal = signal[:num_chunks*self.num_samples]
-        len_post = len(signal)
-        signal = torch.reshape(signal, (num_chunks, self.num_samples))
-        if self.verbose_sample_wasting:
-            print(f'Losed {len_pre - len_post} samples ({((len_pre-len_post)/self.target_sample_rate):.1f}) sec')
-        return signal
-    
-    def adjust_audio_len(self, signal):
-        random_index = random.randint(0, len(signal) - self.num_samples - 1)
-        signal = signal[random_index : random_index + self.num_samples]
-        return signal
         
     
-    def resample(self, signal, sr):
-        if sr != self.target_sample_rate:
-            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
-            signal = resampler(signal)
-        return signal
+    signal, sr = torchaudio.load(wav_file_path)
+    signal = signal.to(device)
+    signal = resample(signal, sr, target_sample_rate)
+    signal = mix_down(signal)
+    if training:
+        # signal = signal.unsqueeze(0)
+        # #signal = signal.to('cpu')
+        # signal = self.augmentation(signal)
+        # #signal = signal.to(self.device)
+        # signal = signal.squeeze(0)
+        
+        signal = adjust_audio_len(signal, num_samples)
+    else:
+        signal = get_signal_chunks(signal, num_samples, target_sample_rate)
+        
+    signal = wav_to_spec_transformation(signal)
+    
+    return signal, sr
 
-    def mix_down(self, signal):
-        signal = torch.mean(signal, dim=0, keepdim=False)
-        return signal
 
 
+
+class GTZANDataset(Dataset):
+
+    def __init__(self,
+                 path_annotations_file,
+                 path_class_to_genre_map,
+                 path_genre_to_class_map,
+                 n_examples='all',
+                 target_sample_rate=None,
+                 device=None,
+                 folds=[0, 1, 2, 3, 4, 5],
+                 training=True,
+                 chunks_len_sec=7.,
+                 verbose_sample_wasting=False,
+                 return_wav_filename=False,
+                 wav_to_spec_transformation=None,
+                 ):
+
+        self.annotations = pd.read_csv(path_annotations_file, index_col=0)
+        with open(path_class_to_genre_map, 'rb') as f:
+            self.class_to_genre_map = pickle.load(f)
+        with open(path_genre_to_class_map, 'rb') as f:
+            self.genre_to_class_map = pickle.load(f)
+
+        self.genres = list(self.genre_to_class_map.keys())
+        self.annotations = self.annotations[self.annotations['fold'].isin(folds)]
+        if n_examples!='all':
+            self.annotations = self.annotations.sample(n_examples, random_state=42)
+        self.training = training
+        self.device = device
+        self.target_sample_rate = target_sample_rate
+        self.num_samples = int(self.target_sample_rate * chunks_len_sec)
+        #self.transformation = transformation.to(device)
+        self.verbose_sample_wasting = verbose_sample_wasting
+        
+        self.wav_to_spec_transformation = wav_to_spec_transformation
+        
+        self.return_wav_filename = return_wav_filename
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        '''
+        self.training == True
+            return torch.Tensor of shape (f, t).
+                f = n_fft
+                t = (self.num_samples) / hop_length
+        self.training == False
+            return torch.Tensor of shape (c, f, t)
+        
+        c = n chunks
+        f = n frequency bins
+        t = n time bins
+        
+        When used with dataloader, batch dimension b has to be added!
+        '''
+        sample_path = self.annotations.iloc[idx]['wav_path']
+        genre = self.annotations.iloc[idx]['genre']
+        label = self.genre_to_class_map[genre]
+        
+        signal, sr = read_wav_and_preprocess(wav_file_path=sample_path,
+                                             wav_to_spec_transformation=self.wav_to_spec_transformation,
+                                             target_sample_rate=self.target_sample_rate,
+                                             num_samples=self.num_samples,
+                                             training=self.training,
+                                             verbose_sample_wasting=self.verbose_sample_wasting,
+                                             device=self.device)
+        
+        if self.return_wav_filename:
+            wav_filename = self.annotations.iloc[idx]['wav_filename']
+            return signal, label, wav_filename
+        return signal, label
+
+
+
+def get_wav_to_spec_transformation(mel_spectrogram_params, target_sample_rate, device):
+    
+        mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+            sample_rate=target_sample_rate,
+            normalized=False,
+            **mel_spectrogram_params,).to(device)
+        amplitude_to_db = torchaudio.transforms.AmplitudeToDB().to(device)
+        
+        wav_to_spec_transformation = [mel_spectrogram, amplitude_to_db]
+        return Compose(wav_to_spec_transformation)
+    
+    
 def create_data_loader(set_='train', batch_size=128, mel_spectrogram_params=None, **dataset_kwargs):
     assert set_ in ['train', 'val', 'test', 'all']
     
@@ -188,8 +215,11 @@ def create_data_loader(set_='train', batch_size=128, mel_spectrogram_params=None
         num_chunks = (avg_n_samples_signal / target_sample_rate) / chunks_len_sec
         batch_size = math.floor(batch_size / num_chunks)
         
+        
+    wav_to_spec_transformation = get_wav_to_spec_transformation(mel_spectrogram_params, dataset_kwargs['target_sample_rate'], dataset_kwargs['device'])
+    
     dataset = GTZANDataset(folds=folds, 
-                           mel_spectrogram_kwargs=mel_spectrogram_params, 
+                           wav_to_spec_transformation=wav_to_spec_transformation,
                            **dataset_kwargs,
                            )
 
@@ -197,7 +227,21 @@ def create_data_loader(set_='train', batch_size=128, mel_spectrogram_params=None
     return dataloader, dataset
 
 
-#%%
+def create_inference_data_loader(wav_file_path, mel_spectrogram_params=None, **dataset_kwargs):
+    target_sample_rate = dataset_kwargs['target_sample_rate']
+    chunks_len_sec = dataset_kwargs['chunks_len_sec']
+    training = dataset_kwargs['training']
+
+    avg_n_samples_signal = 661794  # Adjust this!!!
+    num_chunks = (avg_n_samples_signal / target_sample_rate) / chunks_len_sec
+
+    dataset = GTZANDataset(mel_spectrogram_kwargs=mel_spectrogram_params,
+                           **dataset_kwargs,
+                           )
+    dataloader = DataLoader(dataset, batch_size=1)
+    return dataloader, dataset
+
+
 def main():
     import numpy as np
     from genre_classification.paths import (
